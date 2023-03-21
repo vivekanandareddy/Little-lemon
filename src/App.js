d468e7b9983c92b1f064b1542a6f65a0045a7064
Vivekananda Reddy <vivekreddy35@gmail.com>
2023-03-21 12:40:15 +0000

    Added Synmantic Elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./styles.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Little Lemon</h1>
+      <h1>Little Lemon Restaurant</h1>
+      <Header />
+      <Main />
+      <Footer />
+      
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import  Nav  from "./Nav"
+function Header () {
+    return(
+        <>
+          <Nav />  
+        </>
+    )
+};
+
+export default Header;
